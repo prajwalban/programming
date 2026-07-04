6f890443f7284ca2978faf0b91718f5eada20444
prajwal <125958586+prajwalban@users.noreply.github.com>
2026-07-04 07:19:00 +0530

    Time: 1 ms (89.79%), Space: 43.2 MB (63.7%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int upperCase=0;
+        for(int i=0;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                upperCase++;
+            }
+        }
+        if (upperCase == word.length()
+                || upperCase == 0
+           || (upperCase == 1 && Character.isUpperCase(word.charAt(0)))) {
+           return true;
+        }
+        return false;
+        
+    }
+}
