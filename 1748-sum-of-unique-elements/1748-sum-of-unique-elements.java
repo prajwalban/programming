cd10e30b345187de2f485c514dedcbb558bafb19
prajwal <125958586+prajwalban@users.noreply.github.com>
2026-07-23 15:15:29 +0530

    Time: 2 ms (58.97%), Space: 43.4 MB (7.14%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,19 +1,15 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-        int[] freq = new int[101];
-
-        for (int num : nums) {
-            freq[num]++;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-
-        int sum = 0;
-
-        for (int i = 1; i <= 100; i++) {
-            if (freq[i] == 1) {
-                sum += i;
+         int sum =0;
+        for(int key:map.keySet()){
+            if(map.get(key)==1){
+                sum+=key;
             }
         }
-
-        return sum;
+       return sum; 
     }
-}
+} 
