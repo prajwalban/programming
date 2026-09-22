187980cf4deae02e6368a680cad5d1ef03cc4e2a
prajwal <125958586+prajwalban@users.noreply.github.com>
2026-09-22 12:43:10 +0530

    Time: 4 ms (29.36%), Space: 44.9 MB (56.81%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
@@ -0,0 +1,31 @@
+class Solution {
+     public static String sortString(String s) {
+    int[] count = new int[26];
+    for (char c : s.toCharArray()) {
+        count[c - 'a']++;
+    }
+
+    StringBuilder result = new StringBuilder();
+
+    while (result.length() < s.length()) {
+    
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                result.append((char) ('a' + i));
+                count[i]--;
+            }
+        }
+     
+        for (int i = 25; i >= 0; i--) {
+            if (count[i] > 0) {
+                result.append((char) ('a' + i));
+                count[i]--;
+            }
+        }
+    }
+
+    return result.toString();
+}
+        
+    
+}
